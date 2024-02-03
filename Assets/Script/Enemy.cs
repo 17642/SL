36e4628c6f3c6313312254dad0b0ce7a11d534f0
상빈 <67950060+17642@users.noreply.github.com>
2024-02-03 17:35:55 +0900

    적의 소리 및 시야 감지 구현

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float scanRadius;
+    [SerializeField]
+    private float scanRange;
+    RaycastHit2D hit;
 
+    int playerLayerMask;
+
+    void Start(){
+        playerLayerMask = 1<<LayerMask.NameToLayer("Player");
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        ScanFront();
+        DrawRay();
     }
+
+    void ScanFront(){
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, scanRadius, playerLayerMask);
+
+        foreach (Collider2D collider in colliders){
+            Vector2 direction = (collider.transform.position - transform.position).normalized;
+            float angle = Vector2.Angle(transform.up, direction);
+            if(angle<scanRange){
+                Debug.Log("시야 범위 접근 각도:"+angle);
+            }
+        }
+
+    }
+
+    void DrawRay(){
+        Vector2 FrontVec = transform.up*scanRadius;
+        Debug.DrawRay(transform.position, FrontVec,Color.red);
+        Debug.DrawRay(transform.position, Quaternion.Euler(0,0,scanRange)*FrontVec,Color.red);
+        Debug.DrawRay(transform.position, Quaternion.Euler(0,0,-scanRange)*FrontVec,Color.red);
+    }
+
+    void OnTriggerStay2D(Collider2D other){
+        if(other.CompareTag("PlayerSoundA")){
+            Debug.Log("소리 범위 A와 접촉");
+        }else if(other.CompareTag("PlayerSoundB")){
+            Debug.Log("소리 범위 B와 접촉");
+        }
+    }
+    
 }
